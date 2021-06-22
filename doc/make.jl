760db34c7c89a61e08b5e60c3760ad39a7be11ab
Arun sanganal <74652697+ArunSanganal@users.noreply.github.com>
2021-06-22 17:55:10 +0530

    Update make.jl

--- doc/make.jl
+++ doc/make.jl
@@ -157,14 +157,14 @@ DevDocs = [
         "devdocs/sanitizers.md",
     ],
     "Building Julia" => [
-        "devdocs/build/build.md"
+        "devdocs/build/build.md",
         "devdocs/build/linux.md",
         "devdocs/build/macos.md",
         "devdocs/build/windows.md",
         "devdocs/build/freebsd.md",
         "devdocs/build/arm.md",
         "devdocs/build/distributing.md",
-        "devdocs/build/README.md"
+        "devdocs/build/README.md",
     ]
 ]
 
